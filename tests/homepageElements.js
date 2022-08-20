48e8fe36099d2ab117bca456797e8b36d5772746
Oliver Spinks <oliver-spinks@hotmail.com>
2022-08-20 20:03:09 +0100

    updated homepageElements page

--- tests/homepageElements.js
+++ tests/homepageElements.js
@@ -4,14 +4,19 @@ class homepageElements{
     
 constructor() {
     this.firstElement = Selector('.football-teams-table ')
-    this.heading = Selector('h1').withText('List of all Football Teams')
+    this.homepageHeading = Selector('h1').withText('List of all Football Teams')
     this.loginButton = Selector('a.nav-link.login')
     this.signIn = Selector('button').withText('Sign in')
     this.signOut = Selector('a').withText('Logout')
     this.username = Selector('input#inputUsername')
     this.password = Selector('input#inputPassword')
-    this.logoutButton = Selector('a.nav-link.logout');
+    this.logoutButton = Selector('a.nav-link.logout')
+    this.createButton = Selector('a.btn.btn-primary')
+    this.formTitle = Selector('input#post_title')
+    this.formAttachment = Selector('input#post_attachment')
+    this.formCategory = Selector('select#post_category')
+    this.formSubmit = Selector('button.btn-primary')
+}
+}
 
-}
-}
 export default new homepageElements();
