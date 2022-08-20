934fd51a914a84790b7dbd65debd69c63df28ce8
Oliver Spinks <oliver-spinks@hotmail.com>
2022-08-20 20:00:02 +0100

    updated loginpage tests.

--- tests/loginpage.js
+++ tests/loginpage.js
@@ -5,16 +5,19 @@ fixture `loginpage`
     .page('https://127.0.0.1:8000/login');
 
 test('First test on Football site', async t => {
-    const pageTitle = Selector('.container h1');
+
+    await t 
+    .expect(homepageElements.username.value).eql('', 'input is empty')
+    .typeText(homepageElements.username, "admin")
+    .expect(homepageElements.username.value).eql('admin', 'input contains "admin"');
+
+    await t     
+    .expect(homepageElements.password.value).eql('', 'input is empty')
+    .typeText(homepageElements.password, "000000")
+    .expect(homepageElements.password.value).eql('000000', 'input contains "000000"');
+
     await t
-        .click(homepageElements.loginButton)
-        .click(homepageElements.username)
-        .typeText(homepageElements.username,'admin')
-        .click(homepageElements.password)
-        .typeText(homepageElements.password,'000000')
-        .click(homepageElements.signIn)
-        .click(homepageElements.signOut)
-
-
+    .click(homepageElements.formSubmit)
+    .expect(homepageElements.homepageHeading.innerText).eql('List of all Football Teams');
 
 }); 
